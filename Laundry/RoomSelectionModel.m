8774de47d94550487ba62f8ce3185c2a26842d1f
Andrew Daniels <andrew.daniels714@gmail.com>
2014-03-28 13:21:33 -0400

    Set the campus field of each room to current campus name :school:

--- Laundry/RoomSelectionModel.m
+++ Laundry/RoomSelectionModel.m
@@ -31,12 +31,12 @@
 	
 	HTMLNode *roomListBody = [roomListParser body];
 	
-	// School name
-	HTMLNode *schoolInfo = [[roomListBody findChildrenWithAttribute:@"id" matchingName:@"right_col_hp_cont" allowPartial:NO] objectAtIndex:0];
-	NSString *schoolName = [[schoolInfo findChildOfClass:@"h4"] allContents];
-	schoolName = [[schoolName stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] lowercaseString];
+	// Campus name
+	HTMLNode *campusInfo = [[roomListBody findChildrenWithAttribute:@"id" matchingName:@"right_col_hp_cont" allowPartial:NO] objectAtIndex:0];
+	NSString *campusName = [[campusInfo findChildOfClass:@"h4"] allContents];
+	campusName = [[campusName stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
 	
-	NSLog(@"%@", schoolName);
+	NSLog(@"%@", campusName);
 	
 	
 	
@@ -86,7 +86,7 @@
 		
 		
 		[roomNames insertObject:roomName atIndex:i];
-		[rooms insertObject:[LaundryRoom roomWithName:roomName campus:@"BOWDOIN COLLEGE"ID:roomID] atIndex:i];
+		[rooms insertObject:[LaundryRoom roomWithName:roomName campus:campusName ID:roomID] atIndex:i];
 	}
 	
 	// dictionary to retrieve a room given its name
